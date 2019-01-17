42898d5fa765352c2b148e23c15ecf17976969cb
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-01-17 06:13:17 +0100

    initial commit

---
+++ src/main/java/com/github/pksokolowski/coeditor/CoeditorApplication.java
@@ -0,0 +1,14 @@
+package com.github.pksokolowski.coeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoeditorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/github/pksokolowski/coeditor/CoeditorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.pksokolowski.coeditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoeditorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
