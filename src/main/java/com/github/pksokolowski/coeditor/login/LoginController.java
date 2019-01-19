9844dd2ee64ba97522d24afea756ad27f2601e03
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-01-19 12:52:13 +0100

    code cleanup
    Moved login-related files to login package

--- src/main/java/com/github/pksokolowski/coeditor/login/LoginController.java
+++ src/main/java/com/github/pksokolowski/coeditor/login/LoginController.java
@@ -1,4 +1,4 @@
-package com.github.pksokolowski.coeditor;
+package com.github.pksokolowski.coeditor.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/github/pksokolowski/coeditor/login/LoginService.java
+++ src/main/java/com/github/pksokolowski/coeditor/login/LoginService.java
@@ -1,4 +1,4 @@
-package com.github.pksokolowski.coeditor;
+package com.github.pksokolowski.coeditor.login;
 
 import org.springframework.stereotype.Service;
 
